1a0b95aefe2d340cb7df9cd61c149c09cea7a990
ddzigal1 <ddzigal1@etf.unsa.ba>
2020-11-13 20:42:52 +0100

    Adition of GT for NON-FIRE images for RituPande_kaggle_Fire Detection from CCTV dataset and more

---
+++ RohanRoy_kaggle_Fire Detection Using Surveillance Camera on Roads/Vali/Non-Fire/generate_gt.py
@@ -0,0 +1,18 @@
+import cv2
+import glob, os
+import numpy as np
+PATH = os.path.abspath('')
+
+output = './ground_truth'
+
+dimenzije = []
+
+i=0
+for filename in os.listdir(PATH):
+    image = cv2.imread(PATH+"\\"+filename,0)
+    if(image is None):
+        continue
+    gt = image*0
+    cv2.imwrite(PATH+"\\ground_truth\\"+filename,gt)
+    print(i)
+    i += 1
